c6f77a7fe805cdfcfbeba80cc12ba8646536c984
GitHubRegisteredUser <131602241+GitHubRegisteredUser@users.noreply.github.com>
2023-07-15 21:15:39 +0300

    Hibernate part 1

---
+++ src/HibernateTest/Test1.java
@@ -0,0 +1,30 @@
+package HibernateTest;
+
+import HibernateTest.Entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .buildSessionFactory();
+
+        try {
+            Session session = factory.getCurrentSession();
+            Employee emp = new Employee("Mariya", "Sidorova", "IT", 400);
+            session.beginTransaction();
+            session.save(emp);
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+            System.out.println(emp);
+        } finally {
+            factory.close();
+        }
+    }
+
+}
