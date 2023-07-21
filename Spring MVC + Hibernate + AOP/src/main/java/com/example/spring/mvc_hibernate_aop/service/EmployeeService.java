af1319b4a020d1dc3db74766090b3c5fb742274d
GitHubRegisteredUser <131602241+GitHubRegisteredUser@users.noreply.github.com>
2023-07-21 22:52:58 +0300

    Spring MVC + Hibernate + AOP part 1

---
+++ Spring MVC + Hibernate + AOP/src/main/java/com/example/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.example.spring.mvc_hibernate_aop.service;
+
+import com.example.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+}
